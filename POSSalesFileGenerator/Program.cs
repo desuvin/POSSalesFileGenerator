b6cb71ce4d7e46aa350e5f05420b5f8deb6105ed
Divina Vincent Paul Abriol <vincent@clearlab.com>
2024-11-12 14:47:12 +0800

    Fix double deduction of refund and fix consultation tax didn't minus

--- POSSalesFileGenerator/Program.cs
+++ POSSalesFileGenerator/Program.cs
@@ -13,8 +13,8 @@ namespace POSSalesFileGenerator
         private static DateTime customdate = new DateTime(2024, 02, 01);
         private static DateTime customdate2 = customdate.AddHours(23).AddMinutes(59).AddSeconds(59);
         private static DateTime currentDateTime = DateTime.Now.Date;
-        private static DateTime previousDateTime2 = DateTime.Now.Date.AddDays(-1).AddHours(23).AddMinutes(59).AddSeconds(59);
-        private static DateTime previousDateTime = DateTime.Now.Date.AddDays(-1);
+        private static DateTime previousDateTime2 = DateTime.Now.Date.AddDays(-25).AddHours(23).AddMinutes(59).AddSeconds(59);
+        private static DateTime previousDateTime = DateTime.Now.Date.AddDays(-25);
         private static string machineId = "17000013";
         private static string batchId;
 
@@ -72,19 +72,57 @@ namespace POSSalesFileGenerator
                 decimal Refunded = 0;
 
                 var orders = await orderService.ListAsync(filter);
+
                 var ordercountpercall = 0;
 
-                int RefundedCount = orderRefunds.Where(r => r.UpdatedAt.Value.ToString("HH") == hour).Count();
-                Refunded += orderRefunds.Where(r => r.UpdatedAt.Value.ToString("HH") == hour).Sum(r => r.Refunds.Sum(refund => refund.Transactions.Sum(transaction => transaction.Amount.Value)));
-                Console.WriteLine("Refunded Amount : ", Refunded);
+               
 
+                
+                
+                //remove double refund
+                var refundDeducted = orderRefunds
+                       .Where(r => !orders.Items.Any(o => o.OrderNumber == r.OrderNumber))
+                       .ToList();
+                int RefundedCount = orderRefunds.Where(r => r.UpdatedAt.Value.ToString("HH") == hour).Count();
+                Refunded += refundDeducted.Where(r => r.UpdatedAt.Value.ToString("HH") == hour).Sum(r => r.Refunds.Sum(refund => refund.Transactions.Sum(transaction => transaction.Amount.Value)));
+                //Console.WriteLine("Refunded Amount : ", Refunded);
+               
                 foreach (var orderitem in orders.Items)
                 {
-
+                    int count_eyeexam = 0;
+                    //Console.WriteLine("EYE EXAM START : " + count_eyeexam);
+                    foreach (var item in orderitem.LineItems)
+                    {
+                        if (item.SKU == "8122")
+                        {
+                            count_eyeexam++;
+                        }
+                    }
                     if (hour == orderitem.CreatedAt.Value.ToString("HH"))
                     {
                         receiptCount++;
-                        decimal subGTO = orderitem.CurrentSubtotalPrice.Value - orderitem.CurrentTotalTax.Value;
+                        decimal CurrentSubTotalPrice = orderitem.CurrentSubtotalPrice.Value;
+                        decimal CurrentTotalTax = orderitem.CurrentTotalTax.Value;
+                        double eyeexam_cost = 30.00;
+                        double eyeexam_tax = 2.48;
+                        if (count_eyeexam > 0)
+                        {
+                            Console.WriteLine("CurrentSubTotalPrice" + CurrentSubTotalPrice);
+                            double c_eyeexam_cost = eyeexam_cost * count_eyeexam;
+                            double c_eyeexam_tax = eyeexam_tax * count_eyeexam;
+
+                            CurrentSubTotalPrice -= (decimal)c_eyeexam_cost;
+                            CurrentTotalTax -= (decimal)c_eyeexam_tax;
+
+                            Console.WriteLine("After Deduction CurrentSubTotalPrice" + CurrentSubTotalPrice);
+                        }
+                        if (orderitem.TotalShippingPriceSet.ShopMoney.Amount > 0)
+                        {
+                            CurrentSubTotalPrice -= (decimal)5;
+                        }
+                        decimal subGTO = CurrentSubTotalPrice - CurrentTotalTax;
+                        Console.WriteLine("Order NUmber : " + orderitem.Name + " Order GTO : " + subGTO);
+                        //decimal subGTO = orderitem.CurrentSubtotalPrice.Value - orderitem.CurrentTotalTax.Value;
                         GTO += subGTO;
                         GST += orderitem.CurrentTotalTax.Value;
                         Discount += orderitem.TotalDiscounts.Value;
@@ -111,17 +149,23 @@ namespace POSSalesFileGenerator
 
                     ordercountpercall++;
                     LineNumber += 1000;
+                    //Console.WriteLine("EYE EXAM END : " + count_eyeexam);
                 }
 
                 if (Refunded > 0)
                 {
                     Console.WriteLine(Refunded);
                     GTO -= Refunded;
+                }
+
+                if (RefundedCount > 0)
+                {
+                    Console.WriteLine("Refunded Count : "+RefundedCount);
                     receiptCount += RefundedCount;
                 }
 
-                var gtostring = GTO.ToString("0.00");
-                var gststring = GST.ToString("0.00");
+                var gtostring = GTO.ToString("0.00"); // need to deduct the eye checkup 27.52
+                var gststring = GST.ToString("0.00"); // need to remove the eye checkup tax 2.48
                 var discountstring = Discount.ToString("0.00");
                 var servicechargestring = ServiceCharge.ToString("0.00");
                 var cashstring = Cash.ToString("0.00");
@@ -131,7 +175,7 @@ namespace POSSalesFileGenerator
                 var amexstring = Amex.ToString("0.00");
                 var voucherstring = Voucher.ToString("0.00");
                 var othersstring = Others.ToString("0.00");
-
+                //17000013 | 225 | 15082024 | 15 | 2 | 222.94 | 20.06 | 18.00 | 0.00 | 0 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 0.00 | 222.94 | Y
                 var rowstring = string.Join("|", machineId, batchId, previousDateTime.ToString("ddMMyyyy").ToUpper(), hour, receiptCount, gtostring, gststring, discountstring,
                     servicechargestring, NoOfPax, cashstring, netsstring, visastring, mastercardstring, amexstring, voucherstring, othersstring, GSTRegistered);
 
@@ -139,7 +183,8 @@ namespace POSSalesFileGenerator
                 receiptCount = 0;
                 Refunded = 0;
 
-                string directoryPath = @"C:\Users\robby\Desktop\POSSALESINTEGRATION\SalesFiles";
+                string directoryPath = @"C:\Users\robby\Desktop\POSSALESINTEGRATION\Test";
+                //string directoryPath = @"C:\Users\robby\Desktop\POSSALESINTEGRATION\SalesFiles";
                 string fileName = $"H{machineId}_{previousDateTime.ToString("yyyyMMdd")}.txt";
                 string filePath = Path.Combine(directoryPath, fileName);
                 using (StreamWriter writer = new StreamWriter(filePath, true))
@@ -164,7 +209,7 @@ namespace POSSalesFileGenerator
             var orderRefunds = await orderService.ListAsync(refundfilter);
             foreach (var orderitem in orderRefunds.Items)
             {
-                Console.WriteLine(orderitem.Name);
+                //Console.WriteLine(orderitem.Name);
                 allRefunds.Add(orderitem);
             }
 
@@ -173,7 +218,8 @@ namespace POSSalesFileGenerator
 
         static async Task<string> GetAndUpdateBatchId()
         {
-            string batchIdFilePath = @"C:\Users\robby\Desktop\POSSALESINTEGRATION\BatchID.txt";
+            string batchIdFilePath = @"C:\Users\robby\Desktop\POSSALESINTEGRATION\BatchIDTest.txt";
+            //string batchIdFilePath = @"C:\Users\robby\Desktop\POSSALESINTEGRATION\BatchID.txt";
             string batchId;
             int newBatchId;
 
